d907434c2295c9d506fafe242d67775d8ca294e8
Li Jiang <lijgame@gmail.com>
2020-12-07 15:17:24 +0800

    recon support recon triplet update recon

--- install_recon_pkg.bat
+++ install_recon_pkg.bat
@@ -1,1 +1,3 @@
-vcpkg install --triplet recon eigen3 rapidxml glfw3 glm xerces-c gflags mpir mpfr glew gtest libpng flann tbb tiff libjpeg-turbo libgeotiff boost log4cplus freeimage gumbo qt5 glog tinyxml2 suitesparse ceres[suitesparse] opencv3[cuda,eigen,tbb] --recurse
+set packages=eigen3 rapidxml glfw3 glm xerces-c gflags mpir mpfr glew gtest libpng flann tbb tiff libjpeg-turbo libgeotiff boost log4cplus[unicode] freeimage gumbo qt5 glog tinyxml2 suitesparse ceres[suitesparse] opencv3[eigen]
+vcpkg install --triplet recon %packages% --recurse
+vcpkg export --triplet  recon %packages% --zip
