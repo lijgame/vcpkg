7c04f49c1634ab2b417f75339b26c859bc7bd4fb
Li Jiang <lijgame@gmail.com>
2020-12-10 16:28:15 +0800

    remove not needed stuff

--- export_recon_pkg.bat
+++
@@ -1,1 +0,0 @@
-vcpkg export --triplet recon eigen3 rapidxml glfw3 glm xerces-c gflags mpir mpfr glew gtest libpng flann tbb tiff libjpeg-turbo libgeotiff boost log4cplus freeimage gumbo qt5 glog tinyxml2 suitesparse[metis,cuda] ceres[suitesparse] opencv3[cuda,eigen,tbb] --zip
