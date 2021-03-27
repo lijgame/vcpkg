a1a4c87ffbe3a423357c61fffaff35b1b1db1331
Li Jiang <lijgame@gmail.com>
2021-03-27 09:49:36 +0800

    add protobuf and spdlog for recon

--- install_recon_pkg.bat
+++ install_recon_pkg.bat
@@ -1,3 +1,3 @@
-set packages=eigen3 rapidxml glfw3 glm xerces-c gflags mpir mpfr glew gtest libpng flann tbb tiff libjpeg-turbo libgeotiff boost log4cplus[unicode] freeimage gumbo qt5 glog tinyxml2 suitesparse ceres[suitesparse] opencv3[eigen]
+set packages=eigen3 rapidxml glfw3 glm xerces-c gflags mpir mpfr glew gtest libpng flann tbb tiff libjpeg-turbo libgeotiff boost log4cplus[unicode] freeimage gumbo qt5 glog tinyxml2 suitesparse ceres[suitesparse] opencv3[eigen] protobuf[zlib] spdlog
 vcpkg install --triplet recon %packages% --recurse
 vcpkg export --triplet  recon %packages% --zip
